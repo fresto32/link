16ae81b34be3c5a2398192f55ed38b49274939fe
Taj Pereira <tajpereira@gmail.com>
2020-05-02 10:13:05 +0930

    Separated logic to find face containing point to its own helper file.
    This logic was moved from setOnPlane(...).

---
+++ src/typescript/World/Helpers/FaceContainingPoint.ts
@@ -0,0 +1,70 @@
+import * as THREE from 'three';
+
+/**
+ * Finds a THREE.Face in plane that contains x and z. If none found, null is
+ * returned.
+ *
+ * @param plane The plane to find the face containing the x and z coordinates.
+ * @param x The x coordinate.
+ * @param z The z coordinate.
+ */
+export default function FaceContainingPoint(
+  plane: THREE.Mesh,
+  x: number,
+  z: number
+): THREE.Face3 | null {
+  if (plane.geometry instanceof THREE.BufferGeometry) return null;
+
+  // Find the face that contains the x and z of position...
+  let containingFace: THREE.Face3 | null = null;
+  plane.geometry.faces.some(face => {
+    if (plane.geometry instanceof THREE.BufferGeometry) return;
+    const vertexA = plane.geometry.vertices[face.a];
+    const vertexB = plane.geometry.vertices[face.b];
+    const vertexC = plane.geometry.vertices[face.c];
+
+    if (positionContainedInVertices(x, z, vertexA, vertexB, vertexC)) {
+      containingFace = face;
+      return true;
+    }
+    return false;
+  });
+
+  return containingFace;
+}
+
+// Since we know that the vertices lie in a plane, we can simplify the problem
+// to a 2D triangle problem. If the position lies in the triangle made by
+// vertices A, B, C (of mapping (x,z) not (x,y,z)), we can say that position is
+// contained in this face.
+//
+// The problem is solved by solving the system of equations made by the
+// Barycentric Coordinates.
+//
+// Reference: https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
+function positionContainedInVertices(
+  x: number,
+  z: number,
+  vertexA: THREE.Vector3,
+  vertexB: THREE.Vector3,
+  vertexC: THREE.Vector3
+): boolean {
+  const y = z;
+
+  const x0 = vertexA.x;
+  const y0 = vertexA.z;
+
+  const x1 = vertexB.x;
+  const y1 = vertexB.z;
+
+  const x2 = vertexC.x;
+  const y2 = vertexC.z;
+
+  const Area = 0.5 * (-y1 * x2 + y0 * (-x1 + x2) + x0 * (y1 - y2) + x1 * y2);
+  const s =
+    (1 / (2 * Area)) * (y0 * x2 - x0 * y2 + (y2 - y0) * x + (x0 - x2) * y);
+  const t =
+    (1 / (2 * Area)) * (x0 * y1 - y0 * x1 + (y0 - y1) * x + (x1 - x0) * y);
+
+  return s >= 0 && s <= 1 && t >= 0 && s <= 1 && s + t <= 1;
+}
--- src/typescript/World/Helpers/SetOnPlane.ts
+++ src/typescript/World/Helpers/SetOnPlane.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import FaceContainingPoint from './FaceContainingPoint';
 
 /**
  * Finds the corresponding y on a flat or undulating plane given x and z
@@ -33,21 +34,9 @@ export default function setOnPlane(
   }
 
   // Find the face that contains the x and z of position...
-  let containingFace: THREE.Face3 | null = null;
-  const isInPlane = plane.geometry.faces.some(face => {
-    if (plane.geometry instanceof THREE.BufferGeometry) return;
-    const vertexA = plane.geometry.vertices[face.a];
-    const vertexB = plane.geometry.vertices[face.b];
-    const vertexC = plane.geometry.vertices[face.c];
+  const containingFace = FaceContainingPoint(plane, x, z);
 
-    if (positionContainedInVertices(x, z, vertexA, vertexB, vertexC)) {
-      containingFace = face.clone();
-      return true;
-    }
-    return false;
-  });
-
-  if (!isInPlane) {
+  if (containingFace === null) {
     throw console.error('Position (x,z) is not found in plane.');
   }
 
@@ -109,40 +98,4 @@ export default function setOnPlane(
   if (object !== null) object.position.set(x, y, z);
 
   return y;
-}
-
-// Since we know that the vertices lie in a plane, we can simplify the problem
-// to a 2D triangle problem. If the position lies in the triangle made by
-// vertices A, B, C (of mapping (x,z) not (x,y,z)), we can say that position is
-// contained in this face.
-//
-// The problem is solved by solving the system of equations made by the
-// Barycentric Coordinates.
-//
-// Reference: https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
-function positionContainedInVertices(
-  x: number,
-  z: number,
-  vertexA: THREE.Vector3,
-  vertexB: THREE.Vector3,
-  vertexC: THREE.Vector3
-): boolean {
-  const y = z;
-
-  const x0 = vertexA.x;
-  const y0 = vertexA.z;
-
-  const x1 = vertexB.x;
-  const y1 = vertexB.z;
-
-  const x2 = vertexC.x;
-  const y2 = vertexC.z;
-
-  const Area = 0.5 * (-y1 * x2 + y0 * (-x1 + x2) + x0 * (y1 - y2) + x1 * y2);
-  const s =
-    (1 / (2 * Area)) * (y0 * x2 - x0 * y2 + (y2 - y0) * x + (x0 - x2) * y);
-  const t =
-    (1 / (2 * Area)) * (x0 * y1 - y0 * x1 + (y0 - y1) * x + (x1 - x0) * y);
-
-  return s >= 0 && s <= 1 && t >= 0 && s <= 1 && s + t <= 1;
 }
