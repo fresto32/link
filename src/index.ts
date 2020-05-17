72b64483e50c3aac19df60a9b9bf40284c07104c
Taj Pereira <tajpereira@gmail.com>
2020-05-17 09:51:41 +0930

    Added application settings to index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 /* eslint-disable no-undef */
 import Application from './typescript/Application';
+import ApplicationSettings from './typescript/Settings/Application';
 
-new Application($('#c')[0] as HTMLCanvasElement);
+const settings: ApplicationSettings = {
+  config: {debug: true, showBoundingBoxes: false, touch: false},
+};
+
+new Application($('#c')[0] as HTMLCanvasElement, settings);
