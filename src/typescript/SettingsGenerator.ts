72fba343487f4e6bc35910f891134ef82e84ed17
Taj Pereira <tajpereira@gmail.com>
2020-05-17 13:29:22 +0930

    Standardised the naming of skyboxes.

--- src/typescript/SettingsGenerator.ts
+++ src/typescript/SettingsGenerator.ts
@@ -293,12 +293,12 @@ export default class SettingsGenerator implements ApplicationSettings {
     // Reason: consistent formatting of resources items
     // prettier-ignore
     this.resources.items.push(
-      {name: 'skyboxBk', source: 'src/models/skybox/5/bay_bk.jpg', type: 'texture'},
-      {name: 'skyboxDn', source: 'src/models/skybox/5/bay_dn.jpg', type: 'texture'},
-      {name: 'skyboxFt', source: 'src/models/skybox/5/bay_ft.jpg', type: 'texture'},
-      {name: 'skyboxLf', source: 'src/models/skybox/5/bay_lf.jpg', type: 'texture'},
-      {name: 'skyboxRt', source: 'src/models/skybox/5/bay_rt.jpg', type: 'texture'},
-      {name: 'skyboxUp', source: 'src/models/skybox/5/bay_up.jpg', type: 'texture'}
+      {name: 'skyboxBk', source: 'src/models/skybox/5/bk.jpg', type: 'texture'},
+      {name: 'skyboxDn', source: 'src/models/skybox/5/dn.jpg', type: 'texture'},
+      {name: 'skyboxFt', source: 'src/models/skybox/5/ft.jpg', type: 'texture'},
+      {name: 'skyboxLf', source: 'src/models/skybox/5/lf.jpg', type: 'texture'},
+      {name: 'skyboxRt', source: 'src/models/skybox/5/rt.jpg', type: 'texture'},
+      {name: 'skyboxUp', source: 'src/models/skybox/5/up.jpg', type: 'texture'}
     )
   }
 }
