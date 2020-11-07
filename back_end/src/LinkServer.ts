b5a5e772e64026bcc6c3daa10fb20ff6c3ace5ce
Taj Pereira <tajpereira@gmail.com>
2020-11-08 07:21:36 +1030

    Fix typo.

--- back_end/src/LinkServer.ts
+++ back_end/src/LinkServer.ts
@@ -14,12 +14,12 @@ class LinkServer extends Server {
   constructor() {
     super(true);
 
-    this.setupMiddleware();
+    this.setUpMiddleware();
     this.setupControllers();
     this.setupViews();
   }
 
-  private setupMiddleware(): void {
+  private setUpMiddleware(): void {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({extended: true}));
   }
