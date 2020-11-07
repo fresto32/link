29314b300396f73798af0eec25975315590c2bac
Taj Pereira <tajpereira@gmail.com>
2020-11-08 07:22:14 +1030

    Added some linting.

--- front_end/src/index.ts
+++ front_end/src/index.ts
@@ -4,18 +4,24 @@ import Settings from './typescript/Settings';
 
 const canvas = $('#c')[0] as HTMLCanvasElement;
 
-fetch('http://localhost:8010/proxy/api/next-card').then(response => {
-  if (response.status === OK) {
-    response.json().then(
-      userCard => {
-        new Application(canvas, userCard.card);
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  } else {
+fetch('http://localhost:8010/proxy/api/next-card').then(
+  response => {
+    if (response.status === OK) {
+      response.json().then(
+        userCard => {
+          new Application(canvas, userCard.card);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    } else {
+      console.error(response.status);
+      console.error(response.statusText);
+    }
+  },
+  () => {
     const settings = new Settings();
     new Application(canvas, settings);
   }
-});
+);
