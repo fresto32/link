30df599d87e4589987b045d000cf4ba4eaacca06
Taj Pereira <tajpereira@gmail.com>
2020-12-30 12:44:14 +1030

    Use new saveCard function in fixture service of Card Store.

--- microservices/card-store/service/src/services/fixtures.service.ts
+++ microservices/card-store/service/src/services/fixtures.service.ts
@@ -1,14 +1,17 @@
 import {Injectable} from '@nestjs/common';
-import {DocumentType} from '@typegoose/typegoose';
-import {CardSettings, UserCard} from '@link/schema/src/card';
+import {CardSettings} from '@link/schema/src/card';
 import {CardSettingsGenerator} from '@link/schema/src/generator';
 import {DatabaseService} from './database.service';
+import {RepositoryService} from './repository.service';
 
 export const NUM_CARD_FIXTURES = 20;
 
 @Injectable()
 export class FixturesService {
-  constructor(private databaseService: DatabaseService) {}
+  constructor(
+    private repositoryService: RepositoryService,
+    private databaseService: DatabaseService
+  ) {}
 
   public async add() {
     await this.databaseService.dropDatabase();
@@ -17,33 +20,8 @@ export class FixturesService {
 
   private async addCardFixtures(NumCards: number = NUM_CARD_FIXTURES) {
     for (let i = 0; i < NumCards; i++) {
-      const cardDocument = await this.createCard();
-      await this.createUserCardFor(cardDocument);
+      const card: CardSettings = new CardSettingsGenerator();
+      await this.repositoryService.saveCard(card);
     }
-  }
-
-  private async createCard() {
-    const card: CardSettings = new CardSettingsGenerator();
-    const cardDocument = await this.databaseService.cardSettingsModel.create(
-      card as CardSettings
-    );
-
-    await cardDocument.save();
-    return cardDocument;
-  }
-
-  private async createUserCardFor(cardDocument: DocumentType<CardSettings>) {
-    const userCard = new UserCard();
-    userCard.card = cardDocument._id;
-
-    // Typegoose needs to use any for Ref types, see:
-    // https://typegoose.github.io/typegoose/docs/guides/known-issues#typesmongoose5722-and-higher
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const userCardDocument = await this.databaseService.userCardModel.create<any>(
-      userCard as UserCard
-    );
-
-    await userCardDocument.save();
   }
 }
