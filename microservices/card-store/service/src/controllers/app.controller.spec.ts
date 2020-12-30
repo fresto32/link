3116d247b7463f94ba7ec5df6d3061b8521ec9ca
Taj Pereira <tajpereira@gmail.com>
2020-12-30 12:41:59 +1030

    Add all card store controller end points.

--- microservices/card-store/service/src/controllers/app.controller.spec.ts
+++ microservices/card-store/service/src/controllers/app.controller.spec.ts
@@ -1,21 +1,163 @@
+import {ApiResult} from '@link/schema/src/common';
+import {
+  CardCreated,
+  CardStored,
+  DeleteCardRequested,
+  DeletedCard,
+  GetAllUserCardsRequested,
+  GotAllUserCards,
+  GotNextCard,
+  NextCardRequested,
+} from '@link/schema/src/events/card';
 import {Test, TestingModule} from '@nestjs/testing';
+import {DatabaseService} from './../services/database.service';
+import {RepositoryService} from './../services/repository.service';
 import {AppController} from './app.controller';
 
 describe('AppController', () => {
   let appController: AppController;
+  let repositoryService: RepositoryService;
+  let clientProxyMock: {
+    emit: jest.SpyInstance;
+  };
 
   beforeEach(async () => {
+    clientProxyMock = {
+      emit: jest.fn(() => {}),
+    };
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [],
+      providers: [
+        RepositoryService,
+        DatabaseService,
+        {
+          provide: 'KAFKA',
+          useValue: clientProxyMock,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    repositoryService = app.get<RepositoryService>(RepositoryService);
   });
 
-  describe('root', () => {
-    it('should return "Hello world!"', () => {
-      expect(appController.getHello()).toBe('Hello world!');
+  describe('handleNextCardRequested', () => {
+    it('should emit the result of repositoryService.nextCard()', async done => {
+      const event: NextCardRequested = {
+        uuid: '12345',
+        timestamp: new Date(),
+        source: 'Api Gateway',
+      };
+
+      jest.spyOn(repositoryService, 'nextCard').mockImplementation(async () => {
+        const result: ApiResult = {
+          data: 'Some next card',
+          error: {message: 'Some error'},
+        };
+
+        return result;
+      });
+
+      await appController.handleNextCardRequested(event);
+
+      const result: GotNextCard = clientProxyMock.emit.mock.calls[0][1];
+      expect(result.uuid).toEqual(event.uuid);
+      expect(result.source).toEqual('Card Store');
+      expect(result.userCard).toEqual('Some next card');
+      expect(result.error?.message).toEqual('Some error');
+
+      done();
+    });
+  });
+
+  describe('handleGetAllUserCardsRequested', () => {
+    it('should emit the result of repositoryService.userCards()', async done => {
+      const event: GetAllUserCardsRequested = {
+        uuid: '12345',
+        timestamp: new Date(),
+        source: 'Api Gateway',
+      };
+
+      jest
+        .spyOn(repositoryService, 'userCards')
+        .mockImplementation(async () => {
+          const result: ApiResult = {
+            data: 'Some cards',
+            error: {message: 'Some error'},
+          };
+
+          return result;
+        });
+
+      await appController.handleGetAllUserCardsRequested(event);
+
+      const result: GotAllUserCards = clientProxyMock.emit.mock.calls[0][1];
+      expect(result.uuid).toEqual(event.uuid);
+      expect(result.source).toEqual('Card Store');
+      expect(result.cards).toEqual('Some cards');
+      expect(result.error?.message).toEqual('Some error');
+
+      done();
+    });
+  });
+
+  describe('handleDeleteCardRequested', () => {
+    it('should emit the result of repositoryService.deleteCard()', async done => {
+      const event: DeleteCardRequested = {
+        uuid: '12345',
+        timestamp: new Date(),
+        source: 'Api Gateway',
+        cardId: 'Some cardId',
+      };
+
+      jest.spyOn(repositoryService, 'deleteCard').mockImplementation(() => {
+        const result: ApiResult = {
+          error: {message: 'Some error'},
+        };
+
+        return result;
+      });
+
+      appController.handleDeleteCardRequested(event);
+
+      const result: DeletedCard = clientProxyMock.emit.mock.calls[0][1];
+      expect(result.uuid).toEqual(event.uuid);
+      expect(result.source).toEqual('Card Store');
+      expect(result.error?.message).toEqual('Some error');
+
+      done();
+    });
+  });
+
+  describe('handleCardCreated', () => {
+    it('should emit the result of repositoryService.save()', async done => {
+      const event: CardCreated = {
+        uuid: '12345',
+        timestamp: new Date(),
+        source: 'Api Gateway',
+        // @ts-expect-error: For testing purposes.
+        card: 'Some card',
+      };
+
+      jest.spyOn(repositoryService, 'saveCard').mockImplementation(async () => {
+        const result: ApiResult = {
+          data: 'Some cardId',
+          error: {message: 'Some error'},
+        };
+
+        return result;
+      });
+
+      await appController.handleCardCreated(event);
+
+      const result: CardStored = clientProxyMock.emit.mock.calls[0][1];
+      expect(result.uuid).toEqual(event.uuid);
+      expect(result.source).toEqual('Card Store');
+      expect(result.cardId).toEqual('Some cardId');
+      expect(result.error?.message).toEqual('Some error');
+
+      done();
     });
   });
 });
--- microservices/card-store/service/src/controllers/app.controller.ts
+++ microservices/card-store/service/src/controllers/app.controller.ts
@@ -1,11 +1,82 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Inject} from '@nestjs/common';
+import {
+  CardCreated,
+  CardStored,
+  DeleteCardRequested,
+  DeletedCard,
+  GetAllUserCardsRequested,
+  GotAllUserCards,
+  GotNextCard,
+  NextCardRequested,
+} from '@link/schema/src/events/card';
+import {ClientProxy, EventPattern} from '@nestjs/microservices';
+import {EventPatterns} from '@link/schema/src/events';
+import {RepositoryService} from './../services/repository.service';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(
+    @Inject('KAFKA') private client: ClientProxy,
+    private repositoryService: RepositoryService
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return 'Hello world!';
+  @EventPattern(EventPatterns.NextCardRequested)
+  async handleNextCardRequested(event: NextCardRequested) {
+    const result = await this.repositoryService.nextCard();
+
+    const eventToEmit: GotNextCard = {
+      uuid: event.uuid,
+      timestamp: new Date(),
+      source: 'Card Store',
+      userCard: result.data,
+      error: result.error,
+    };
+
+    this.client.emit(EventPatterns.GotNextCard, eventToEmit);
+  }
+
+  @EventPattern(EventPatterns.GetAllUserCardsRequested)
+  async handleGetAllUserCardsRequested(event: GetAllUserCardsRequested) {
+    const result = await this.repositoryService.userCards();
+
+    const eventToEmit: GotAllUserCards = {
+      uuid: event.uuid,
+      timestamp: new Date(),
+      source: 'Card Store',
+      cards: result.data,
+      error: result.error,
+    };
+
+    this.client.emit(EventPatterns.GotAllUserCards, eventToEmit);
+  }
+
+  @EventPattern(EventPatterns.DeleteCardRequested)
+  handleDeleteCardRequested(event: DeleteCardRequested) {
+    const result = this.repositoryService.deleteCard(event.cardId);
+
+    const eventToEmit: DeletedCard = {
+      uuid: event.uuid,
+      timestamp: new Date(),
+      source: 'Card Store',
+      cardId: event.cardId,
+      error: result.error,
+    };
+
+    this.client.emit(EventPatterns.DeletedCard, eventToEmit);
+  }
+
+  @EventPattern(EventPatterns.CardCreated)
+  async handleCardCreated(event: CardCreated) {
+    const result = await this.repositoryService.saveCard(event.card);
+
+    const eventToEmit: CardStored = {
+      uuid: event.uuid,
+      timestamp: new Date(),
+      source: 'Card Store',
+      cardId: result.data,
+      error: result.error,
+    };
+
+    this.client.emit(EventPatterns.CardStored, eventToEmit);
   }
 }
