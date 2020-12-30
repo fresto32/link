b3b4f4fb318efe770b20525f7edec22af2b2196e
Taj Pereira <tajpereira@gmail.com>
2020-12-30 12:43:53 +1030

    Save saveCard method to card store repository.

--- microservices/card-store/service/src/services/repository.service.ts
+++ microservices/card-store/service/src/services/repository.service.ts
@@ -1,5 +1,6 @@
+import {CardSettings, UserCard} from '@link/schema/src/card';
+import {ApiResult} from '@link/schema/src/common';
 import {Injectable} from '@nestjs/common';
-import {UserCard} from '@link/schema/src/card';
 import {DatabaseService} from './database.service';
 
 @Injectable()
@@ -7,18 +8,79 @@ export class RepositoryService {
   constructor(private databaseService: DatabaseService) {}
 
   public async userCards() {
-    return await this.databaseService.userCardModel
-      .find()
-      .populate('cards')
-      .exec();
+    const result: ApiResult = {};
+
+    try {
+      result.data = await this.databaseService.userCardModel
+        .find()
+        .populate('cards')
+        .exec();
+    } catch (error) {
+      result.error = {};
+      result.error.message = `Error when retrieving all cards: ${error}`;
+    }
+
+    return result;
   }
 
-  public async nextCard(): Promise<UserCard | null> {
-    return this.databaseService.userCardModel
-      .findOne()
-      .sort({dueDate: 'ascending'})
-      .populate('card')
-      .lean()
-      .exec();
+  public async nextCard(): Promise<ApiResult> {
+    const result: ApiResult = {};
+
+    try {
+      result.data = await this.databaseService.userCardModel
+        .findOne()
+        .sort({dueDate: 'ascending'})
+        .populate('card')
+        .lean()
+        .exec();
+    } catch (error) {
+      result.error = {};
+      result.error.message = `Error when retrieving next card: ${error}`;
+    }
+
+    return result;
+  }
+
+  public async saveCard(card: CardSettings): Promise<ApiResult> {
+    const result: ApiResult = {};
+
+    try {
+      const cardDocument = await this.databaseService.cardSettingsModel.create(
+        card as CardSettings
+      );
+      await cardDocument.save();
+
+      const userCard = new UserCard();
+      userCard.card = cardDocument._id;
+      // Typegoose needs to use any for Ref types, see:
+      // https://typegoose.github.io/typegoose/docs/guides/known-issues#typesmongoose5722-and-higher
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const userCardDocument = await this.databaseService.userCardModel.create<any>(
+        userCard as UserCard
+      );
+
+      await userCardDocument.save();
+    } catch (error) {
+      result.error = {
+        message: `Error when saving card: ${error.errmsg}`,
+      };
+    }
+
+    return result;
+  }
+
+  public deleteCard(cardId: string): ApiResult {
+    const result: ApiResult = {};
+
+    try {
+      this.databaseService.userCardModel.deleteOne({_id: cardId});
+    } catch (error) {
+      result.error = {
+        message: `Error when deleting card: ${error.errmsg}`,
+      };
+    }
+
+    return result;
   }
 }
