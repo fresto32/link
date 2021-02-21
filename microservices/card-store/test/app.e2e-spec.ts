44f888856009ca859d94e94d5aba2ad2c51f218f
Taj Pereira <tajpereira@gmail.com>
2021-02-21 12:35:23 +1030

    Initial successful pass of E2E testing of card storage.

--- microservices/card-store/test/app.e2e-spec.ts
+++ microservices/card-store/test/app.e2e-spec.ts
@@ -1,61 +1,159 @@
-import { Test, TestingModule } from "@nestjs/testing";
 import { INestMicroservice } from "@nestjs/common";
-import { CardStoreModule } from "./../src/card-store.module";
 import { DatabaseService } from "./../src/services/database.service";
 import { RepositoryService } from "./../src/services/repository.service";
-import { EventEmitter2 } from "@nestjs/event-emitter";
-import { ClientProxy, ClientsModule, Transport } from "@nestjs/microservices";
-import { AppConfigService } from "./../src/configuration/config.service";
 import { environmentConfig } from "./../src/configuration/config";
-import { ConfigService as NestConfigService } from "@nestjs/config";
+import {
+  CardCreated,
+  CardStored,
+  DeleteCardRequested,
+  DeletedCard,
+  CardEvent,
+  GetAllUserCardsRequested,
+  GotAllUserCards,
+  GotNextCard,
+  NextCardRequested,
+  EventPatterns,
+} from "@link/schema/src/events/card";
+import { CardSettingsGenerator } from "@link/schema/build/src/generator";
+import { bootstrap } from "./../src/bootstrap";
+import { Consumer, Kafka, Producer } from "kafkajs";
+import { ClientProxy } from "@nestjs/microservices";
 
-const KAFKA_BROKER = environmentConfig().kafka.broker.url;
+describe("Card Store Microservice (e2e)", () => {
+  let kafkaMessagesLog = new Map<string, any>();
+  let kafka: Kafka;
+  let producer: Producer;
+  let consumer: Consumer;
 
-describe("AppController (e2e)", () => {
-  let app: INestMicroservice;
-  let client: ClientProxy;
+  // Build Kafka messaging consumer and producer for E2E testing.
+  beforeAll(async () => {
+    const KAFKA_BROKER = environmentConfig().kafka.broker.url;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        CardStoreModule,
-        ClientsModule.register([
-          {
-            name: "KAFKA",
-            transport: Transport.KAFKA,
-            options: {
-              client: {
-                ssl: false,
-                brokers: [KAFKA_BROKER],
-              },
-            },
-          },
-        ]),
-      ],
-      providers: [
-        RepositoryService,
-        DatabaseService,
-        EventEmitter2,
-        AppConfigService,
-        NestConfigService,
-      ],
-    }).compile();
+    kafkaMessagesLog = new Map<string, any>();
 
-    app = moduleFixture.createNestMicroservice({
-      client: {
-        ssl: false,
-        brokers: [KAFKA_BROKER],
-      },
+    kafka = new Kafka({
+      clientId: "Card Store",
+      brokers: [KAFKA_BROKER],
     });
 
-    await app.init();
+    producer = kafka.producer();
 
-    client = app.get("KAFKA");
-    await client.connect();
+    consumer = kafka.consumer({ groupId: "E2E Testing Consumer" });
+
+    await consumer.connect().then(() => {
+      consumer.subscribe({ topic: "card", fromBeginning: false }).then(() => {
+        consumer.run({
+          eachMessage: async ({ topic, partition, message }) => {
+            kafkaMessagesLog.set(
+              message.key?.toString(),
+              message.value?.toString()
+            );
+          },
+        });
+      });
+    });
+
+    await producer.connect();
   });
 
-  it("stores cards into the database", () => {});
-  it("deletes cards from the database", () => {});
-  it("gets all user cards", () => {});
-  it("retrieves the next card", () => {});
+  let app: INestMicroservice;
+  let repositoryService: RepositoryService;
+  let databaseService: DatabaseService;
+  let client: ClientProxy;
+
+  // Set up App testing infrastructure.
+  beforeEach(async () => {
+    // Increase timeout to allow for Kafka rebalancing not causing failed tests.
+    jest.setTimeout(40000);
+
+    app = await bootstrap();
+
+    repositoryService = app.get<RepositoryService>(RepositoryService);
+    databaseService = app.get<DatabaseService>(DatabaseService);
+    client = app.get<ClientProxy>("KAFKA");
+
+    // It takes time for Kafka to register (and possibly rebalance for) the new
+    // NestJS connection.
+    await waitFor(5000);
+
+    // Reset database on start
+    await databaseService.dropDatabase();
+  });
+
+  afterEach(async (done) => {
+    await client.close();
+    await app.close();
+
+    done();
+  });
+
+  afterAll(async (done) => {
+    await Promise.all([producer.disconnect(), consumer.disconnect()]);
+    await client.close();
+    await app.close();
+
+    done();
+  });
+
+  describe("Card storage", () => {
+    const cardUuid = "Uuid for card created event";
+
+    beforeEach(async () => {
+      const cardCreated: CardCreated = {
+        uuid: cardUuid,
+        timestamp: new Date(),
+        source: "Api Gateway",
+        card: new CardSettingsGenerator(),
+      };
+
+      const cardEvent: CardEvent = {
+        pattern: EventPatterns.cardCreated,
+        payload: cardCreated,
+      };
+
+      // Reset database on start
+      await databaseService.dropDatabase();
+
+      // Start with a clean kafkaMessagesLog.
+      kafkaMessagesLog.clear();
+
+      await producer.send({
+        topic: "card",
+        messages: [{ key: cardUuid, value: JSON.stringify(cardEvent) }],
+      });
+    });
+
+    it("stores the card within a CardCreated event in mongo", async (done) => {
+      // It takes time for Kafka to send the message and for the Card Store to
+      // register its arrival and perform relevant computation.
+      await waitFor(5000);
+
+      const userCards = (await repositoryService.userCards()).data;
+      expect(userCards).toHaveLength(1);
+
+      done();
+    });
+
+    it("emits a card stored event to Kafka", async (done) => {
+      // It takes time for Kafka to send the message and for the Card Store to
+      // register its arrival and perform relevant computation.
+      await waitFor(5000);
+
+      const cardStoredEvent = JSON.parse(kafkaMessagesLog.get(cardUuid));
+      expect(cardStoredEvent).toBeTruthy();
+
+      expect(cardStoredEvent.pattern).toEqual(EventPatterns.cardStored);
+      expect(cardStoredEvent.payload.source).toEqual("Card Store");
+      expect(cardStoredEvent.payload.uuid).toEqual(cardUuid);
+
+      done();
+    });
+  });
 });
+
+/**
+ * Pauses on the current line for `milliseconds`.
+ */
+async function waitFor(milliseconds: number) {
+  await new Promise<void>((r) => setTimeout(() => r(), milliseconds));
+}
