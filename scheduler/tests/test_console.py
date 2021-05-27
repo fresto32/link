4e04a97d25cb91b5c141eb1fb3d2b5f8de3064cd
addacub <addacu@gmail.com>
2021-05-27 19:32:04 +0930

    Completed Example CLI: Handling exceptions gracefully

--- scheduler/tests/test_console.py
+++ scheduler/tests/test_console.py
@@ -1,6 +1,6 @@
 # tests/test_console.py
 import click.testing
-import pytest
+import pytest, requests
 
 from scheduler import console
 
@@ -33,3 +33,10 @@ def test_main_fails_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
+
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert "Error" in result.output
+
+
