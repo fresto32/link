7a8a9c4acd9aa08850436f4e835741167e01eff5
addacub <addacu@gmail.com>
2021-06-09 19:28:39 +0930

    Finished Chapter 4 - typing. Also discovered I was missing some code that I copied in from the authors sample code

--- scheduler/noxfile.py
+++ scheduler/noxfile.py
@@ -1,63 +1,16 @@
 # noxfile.py
 import tempfile
+from typing import Any
 
 import nox
+from nox.sessions import Session
 
-
+package = "scheduler"
 nox.options.sessions = "lint", "mypy", "tests"
-
-
-@nox.session(python=["3.8", "3.7"])
-def tests(session):
-    args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(
-        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
-    )
-    session.run("pytest", *args)
-
-
 locations = "src", "tests", "noxfile.py"
 
 
-@nox.session(python=["3.8", "3.7"])
-def lint(session):
-    args = session.posargs or locations
-    install_with_constraints(
-        session,
-        "flake8",
-        "flake8-bandit",
-        "flake8-black",
-        "flake8-bugbear",
-        "flake8-import-order",
-    )
-    session.run("flake8", *args)
-
-
-@nox.session(python="3.8")
-def black(session):
-    args = session.posargs or locations
-    install_with_constraints(session, "black")
-    session.run("black", *args)
-
-
-@nox.session(python="3.8")
-def safety(session):
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        install_with_constraints(session, "safety")
-        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
-
-
-def install_with_constraints(session, *args, **kwargs):
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -74,7 +27,71 @@ def install_with_constraints(session, *args, **kwargs):
 
 
 @nox.session(python="3.8")
-def mypy(session):
+def black(session: Session) -> None:
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+
+
+@nox.session(python=["3.8", "3.7"])
+def lint(session: Session) -> None:
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def safety(session: Session) -> None:
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python="3.8")
+def mypy(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(session, "mypy")
     session.run("mypy", *args)
+
+
+# Only works with Python 3.7
+# @nox.session(python="3.7")
+# def pytype(session: Session) -> None:
+#     args = session.posargs or ["--disable=import-error", *locations]
+#     install_with_constraints(session, "pytype")
+#     session.run("pytype", *args)
+
+
+@nox.session(python=["3.8", "3.7"])
+def tests(session: Session) -> None:
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
+    session.run("pytest", *args)
+
+
+# Position of typeguard
+@nox.session(python=["3.8", "3.7"])
+def typeguard(session):
+    args = session.posargs or ["-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "pytest", "pytest-mock", "typeguard")
+    session.run("pytest", f"--typeguard-packages={package}", *args)
--- scheduler/tests/test_console.py
+++ scheduler/tests/test_console.py
@@ -1,64 +1,79 @@
 # tests/test_console.py
-import click.testing
+from unittest.mock import Mock
+
+from click.testing import CliRunner
 import pytest
+from pytest_mock import MockFixture
 import requests
 
 from scheduler import console
 
 
 @pytest.fixture
-def runner():
-    return click.testing.CliRunner()
+def runner() -> CliRunner:
+    return CliRunner()
 
 
 @pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
+def mock_wikipedia_random_page(mocker: MockFixture) -> Mock:
+    return mocker.patch("scheduler.wikipedia.random_page")
 
 
-def test_main_suceeds(runner, mock_requests_get):
+def test_main_suceeds(runner: CliRunner, mock_requests_get: Mock) -> None:
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
-def test_main_prints_title(runner, mock_requests_get):
+@pytest.mark.e2e
+def test_main_succeeds_in_production_env(runner: CliRunner) -> None:
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
+
+
+def test_main_prints_title(runner: CliRunner, mock_requests_get: Mock) -> None:
     result = runner.invoke(console.main)
     assert "Lorem Ipsum" in result.output
 
 
-def test_main_invokes_requests_get(runner, mock_requests_get):
+def test_main_invokes_requests_get(runner: CliRunner, mock_requests_get: Mock) -> None:
     runner.invoke(console.main)
     assert mock_requests_get.called
 
 
-def test_main_fails_on_request_error(runner, mock_requests_get):
+def test_main_uses_en_wikipedia_org(runner: CliRunner, mock_requests_get: Mock) -> None:
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
+
+
+def test_main_uses_specified_language(
+    runner: CliRunner, mock_wikipedia_random_page: Mock
+) -> None:
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
+
+
+def test_main_fails_on_request_error(
+    runner: CliRunner, mock_requests_get: Mock
+) -> None:
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
 
 
-def test_main_prints_message_on_request_error(runner, mock_requests_get):
+def test_main_prints_message_on_request_error(
+    runner: CliRunner, mock_requests_get: Mock
+) -> None:
     mock_requests_get.side_effect = requests.RequestException
     result = runner.invoke(console.main)
     assert "Error" in result.output
 
 
-@pytest.fixture
-def mock_wikipedia_random_page(mocker):
-    return mocker.patch("scheduler.wikipedia.random_page")
-
-
-def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
-    runner.invoke(console.main, ["--language=pl"])
-    mock_wikipedia_random_page.assert_called_with(language="pl")
-
-
-@pytest.mark.e2e
-def test_main_succeeds_in_production_env(runner):
-    result = runner.invoke(console.main)
-    assert result.exit_code == 0
+# @pytest.fixture
+# def mock_requests_get(mocker):
+#     mock = mocker.patch("requests.get")
+#     mock.return_value.__enter__.return_value.json.return_value = {
+#         "title": "Lorem Ipsum",
+#         "extract": "Lorem ipsum dolor sit amet",
+#     }
+#     return mock
